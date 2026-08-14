1caa0618035c81eae790ad5aaa3392cb0e9faf65
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-14 18:14:58 +0300

    Remove obsolete AlphaSodiumWaterMixin stub (Sodium honours the Fabric fluid handler)

--- src/main/java/net/nostalgia/mixin/alpha/AlphaSodiumWaterMixin.java
+++
@@ -1,4 +0,0 @@
-package net.nostalgia.mixin.alpha;
-// PORT NOTE: Disabled - fluid rendering API changed between 26.1 and 1.21.11.
-// FluidModel/BlockTintSource/FluidStateModelSet are 26.1-only. Will be ported separately.
-public class AlphaSodiumWaterMixin {}
