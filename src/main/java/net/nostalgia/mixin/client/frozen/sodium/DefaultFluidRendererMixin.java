ccc70bf2f47edd0dae8701d70a19619dd46ddf37
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-14 18:15:17 +0300

    Remove obsolete DefaultFluidRendererMixin stub (frozen water sprites moved into AlphaFluidRendering)

--- src/main/java/net/nostalgia/mixin/client/frozen/sodium/DefaultFluidRendererMixin.java
+++
@@ -1,4 +0,0 @@
-package net.nostalgia.mixin.client.frozen.sodium;
-// PORT NOTE: Disabled - FluidModel does not exist in Sodium 0.8.7 for 1.21.11.
-// DefaultFluidRenderer frozen sprite swap will be re-implemented when Sodium API mapping is done.
-public class DefaultFluidRendererMixin {}
