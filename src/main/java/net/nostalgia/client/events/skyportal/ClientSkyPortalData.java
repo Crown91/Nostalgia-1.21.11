59b94416f78024f49a22bf088933379ec8e52b7a
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-15 21:11:52 +0300

    feat(update): classic clouds/stars renderers, client block update hologram sync, sky portal data

--- src/main/java/net/nostalgia/client/events/skyportal/ClientSkyPortalData.java
+++ src/main/java/net/nostalgia/client/events/skyportal/ClientSkyPortalData.java
@@ -4,12 +4,12 @@ import java.util.UUID;
 import net.minecraft.core.BlockPos;
 
 public record ClientSkyPortalData(
-        UUID id,
-        BlockPos center,
-        int crackPlaneY,
-        int crackPlaneYTarget,
-        boolean inverted,
-        long seed,
-        String sourceDimension,
-        String targetDimension
+  UUID id,
+  BlockPos center,
+  int crackPlaneY,
+  int crackPlaneYTarget,
+  boolean inverted,
+  long seed,
+  String sourceDimension,
+  String targetDimension
 ) {}
