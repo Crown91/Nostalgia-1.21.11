1bce3d7546496c32d189c2e6f79a553a326799af
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-16 20:29:49 +0300

    update: upstream sync (EpilepsyWarningScreen, adapted to GuiGraphics/render)

--- src/main/java/net/nostalgia/client/gui/EpilepsyWarningScreen.java
+++ src/main/java/net/nostalgia/client/gui/EpilepsyWarningScreen.java
@@ -1,68 +1,92 @@
 package net.nostalgia.client.gui;
 
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.Identifier;
 import java.io.File;
 import java.io.IOException;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.resources.Identifier;
 
 public class EpilepsyWarningScreen extends Screen {
+  private static final Identifier FLASH_TEXTURE = Identifier.fromNamespaceAndPath("nostalgia", "textures/environment/flash.png");
+  private final Screen parent;
+  public static final File CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("nostalgia_warning_accepted.txt").toFile();
 
-    private static final Identifier FLASH_TEXTURE = Identifier.fromNamespaceAndPath("nostalgia", "textures/environment/flash.png");
-    private final Screen parent;
-    public static final File CONFIG_FILE = net.fabricmc.loader.api.FabricLoader.getInstance().getConfigDir().resolve("nostalgia_warning_accepted.txt").toFile();
+  public EpilepsyWarningScreen(Screen parent) {
+    super(Component.translatable("gui.nostalgia.warning.title"));
+    this.parent = parent;
+  }
 
-    public EpilepsyWarningScreen(Screen parent) {
-        super(Component.translatable("gui.nostalgia.warning.title"));
-        this.parent = parent;
+  public static boolean hasAcceptedWarning() {
+    return CONFIG_FILE.exists();
+  }
+
+  private void acceptWarning() {
+    try {
+      if (!CONFIG_FILE.getParentFile().exists()) {
+        CONFIG_FILE.getParentFile().mkdirs();
+      }
+
+      CONFIG_FILE.createNewFile();
+    } catch (IOException var2) {
+      var2.printStackTrace();
     }
+  }
 
-    public static boolean hasAcceptedWarning() { return CONFIG_FILE.exists(); }
+  protected void init() {
+    super.init();
+  }
 
-    private void acceptWarning() {
-        try {
-            if (!CONFIG_FILE.getParentFile().exists()) CONFIG_FILE.getParentFile().mkdirs();
-            CONFIG_FILE.createNewFile();
-        } catch (IOException e) { e.printStackTrace(); }
+  public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
+    double mouseX = event.x();
+    double mouseY = event.y();
+    int btnW = 130;
+    int btnH = 30;
+    int btnX = this.width / 2 - btnW / 2;
+    int btnY = this.height / 2 + 45;
+    if (mouseX >= btnX && mouseX <= btnX + btnW && mouseY >= btnY && mouseY <= btnY + btnH) {
+      this.acceptWarning();
+      if (this.minecraft != null) {
+        this.minecraft.setScreen(this.parent);
+      }
+
+      return true;
+    } else {
+      return super.mouseClicked(event, doubleClick);
     }
+  }
 
-    @Override
-    protected void init() { super.init(); }
+  public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+    int textureWidth = 256;
+    int textureHeight = 256;
+    int x = (this.width - textureWidth) / 2;
+    int y = (this.height - textureHeight) / 2;
+    graphics.blit(RenderPipelines.GUI_TEXTURED, FLASH_TEXTURE, x, y, 0.0F, 0.0F, textureWidth, textureHeight, textureWidth, textureHeight);
+    Component boldTitle = this.title.copy().withStyle(Style.EMPTY.withBold(true));
+    graphics.drawCenteredString(this.font, boldTitle, this.width / 2, y + 25, -1);
+    Component warningText = Component.translatable("gui.nostalgia.warning.text").copy().withStyle(Style.EMPTY.withBold(true));
+    int textY = y + 108;
+    graphics.drawWordWrap(this.font, warningText, this.width / 2 - 85, textY, 170, -1, true);
+    int btnW = 130;
+    int btnH = 30;
+    int btnX = this.width / 2 - btnW / 2;
+    int btnY = this.height / 2 + 45;
+    boolean hovered = mouseX >= btnX && mouseX <= btnX + btnW && mouseY >= btnY && mouseY <= btnY + btnH;
+    int btnColor = hovered ? -11141121 : -1;
+    Component continueText = Component.translatable("gui.nostalgia.warning.continue").copy().withStyle(Style.EMPTY.withBold(true));
+    graphics.drawCenteredString(this.font, continueText, this.width / 2, btnY + 11, btnColor);
+    super.render(graphics, mouseX, mouseY, partialTick);
+  }
 
-    @Override
-    public boolean mouseClicked(net.minecraft.client.input.MouseButtonEvent event, boolean doubleClick) {
-        double mouseX = event.x(), mouseY = event.y();
-        int btnW = 130, btnH = 30, btnX = this.width / 2 - btnW / 2, btnY = this.height / 2 + 45;
-        if (mouseX >= btnX && mouseX <= btnX + btnW && mouseY >= btnY && mouseY <= btnY + btnH) {
-            acceptWarning();
-            if (this.minecraft != null) this.minecraft.setScreen(this.parent);
-            return true;
-        }
-        return super.mouseClicked(event, doubleClick);
-    }
+  public void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+    super.renderBackground(graphics, mouseX, mouseY, partialTick);
+  }
 
-    @Override
-    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        int textureWidth = 256, textureHeight = 256;
-        int x = (this.width - textureWidth) / 2, y = (this.height - textureHeight) / 2;
-        graphics.blit(net.minecraft.client.renderer.RenderPipelines.GUI_TEXTURED, FLASH_TEXTURE, x, y, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
-        Component boldTitle = this.title.copy().withStyle(net.minecraft.network.chat.Style.EMPTY.withBold(true));
-        graphics.drawCenteredString(this.font, boldTitle, this.width / 2, y + 25, 0xFFFFFFFF);
-        Component warningText = Component.translatable("gui.nostalgia.warning.text").copy().withStyle(net.minecraft.network.chat.Style.EMPTY.withBold(true));
-        graphics.drawWordWrap(this.font, warningText, this.width / 2 - 85, y + 108, 170, 0xFFFFFFFF, true);
-        int btnW = 130, btnH = 30, btnX = this.width / 2 - btnW / 2, btnY = this.height / 2 + 45;
-        boolean hovered = mouseX >= btnX && mouseX <= btnX + btnW && mouseY >= btnY && mouseY <= btnY + btnH;
-        Component continueText = Component.translatable("gui.nostalgia.warning.continue").copy().withStyle(net.minecraft.network.chat.Style.EMPTY.withBold(true));
-        graphics.drawCenteredString(this.font, continueText, this.width / 2, btnY + 11, hovered ? 0xFF55FFFF : 0xFFFFFFFF);
-        super.render(graphics, mouseX, mouseY, partialTick);
-    }
-
-    @Override
-    public void renderBackground(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-        super.renderBackground(graphics, mouseX, mouseY, partialTick);
-    }
-
-    @Override
-    public boolean shouldCloseOnEsc() { return false; }
+  public boolean shouldCloseOnEsc() {
+    return false;
+  }
 }
