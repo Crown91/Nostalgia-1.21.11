b243b6dbf220f41b7dab285a434587c37443edaf
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-16 15:05:57 +0300

    update: sync ModBlockEntities formatting with upstream

--- src/main/java/net/nostalgia/block/entity/ModBlockEntities.java
+++ src/main/java/net/nostalgia/block/entity/ModBlockEntities.java
@@ -4,17 +4,21 @@ import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityT
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.nostalgia.NostalgiaMod;
 import net.nostalgia.block.ModBlocks;
 
 public class ModBlockEntities {
-    public static BlockEntityType<TimeMachineBlockEntity> TIME_MACHINE_BE;
+  public static BlockEntityType<TimeMachineBlockEntity> TIME_MACHINE_BE;
 
-    public static void registerBlockEntities() {
-        TIME_MACHINE_BE = Registry.register(
-                BuiltInRegistries.BLOCK_ENTITY_TYPE,
-                Identifier.fromNamespaceAndPath(NostalgiaMod.MOD_ID, "time_machine_be"),
-                FabricBlockEntityTypeBuilder.create(TimeMachineBlockEntity::new, ModBlocks.TIME_MACHINE).build());
-    }
+  public ModBlockEntities() {
+  }
+
+  public static void registerBlockEntities() {
+    TIME_MACHINE_BE = (BlockEntityType<TimeMachineBlockEntity>)Registry.register(
+      BuiltInRegistries.BLOCK_ENTITY_TYPE,
+      Identifier.fromNamespaceAndPath("nostalgia", "time_machine_be"),
+      FabricBlockEntityTypeBuilder.create(TimeMachineBlockEntity::new, new Block[]{ModBlocks.TIME_MACHINE}).build()
+    );
+  }
 }
