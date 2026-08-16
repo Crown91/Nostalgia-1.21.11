07d163bdf67b79b0beb263159366658c15e48a72
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-16 16:11:36 +0300

    update: upstream sync (FrozenSpriteRegistry, CoordinateMapper, GravityGlowLayer)

--- src/main/java/net/nostalgia/alphalogic/ritual/CoordinateMapper.java
+++ src/main/java/net/nostalgia/alphalogic/ritual/CoordinateMapper.java
@@ -1,108 +1,25 @@
 package net.nostalgia.alphalogic.ritual;
 
 import net.minecraft.core.BlockPos;
+import net.nostalgia.alphalogic.ritual.geometry.TransitionGeometry;
 
 public final class CoordinateMapper {
+  private CoordinateMapper() {
+  }
 
-    private CoordinateMapper() {}
+  public static BlockPos forward(TransitionGeometry geo, BlockPos pos) {
+    return geo.forward(pos);
+  }
 
-    public static BlockPos sourceToTarget(BlockPos sourcePos, int offsetX, int yOffset, int offsetZ, boolean inverted, int maxY) {
-        int ax = sourcePos.getX() + offsetX;
-        int ay;
-        int az;
-        if (inverted) {
-            SkyPortalEventInstance portal = SkyPortalManager.getActive();
-            if (portal != null) {
-                ay = portal.crackPlaneY() + portal.crackPlaneYTarget() - sourcePos.getY();
-                az = 2 * portal.center().getZ() - sourcePos.getZ();
-            } else {
-                ay = maxY - sourcePos.getY();
-                az = sourcePos.getZ() + offsetZ;
-            }
-        } else {
-            ay = sourcePos.getY() - yOffset;
-            az = sourcePos.getZ() + offsetZ;
-        }
-        return new BlockPos(ax, ay, az);
-    }
+  public static BlockPos inverse(TransitionGeometry geo, BlockPos pos) {
+    return geo.inverse(pos);
+  }
 
-    public static BlockPos sourceToTarget(int wx, int wy, int wz, int offsetX, int yOffset, int offsetZ, boolean inverted, int maxY) {
-        int ax = wx + offsetX;
-        int ay;
-        int az;
-        if (inverted) {
-            SkyPortalEventInstance portal = SkyPortalManager.getActive();
-            if (portal != null) {
-                ay = portal.crackPlaneY() + portal.crackPlaneYTarget() - wy;
-                az = 2 * portal.center().getZ() - wz;
-            } else {
-                ay = maxY - wy;
-                az = wz + offsetZ;
-            }
-        } else {
-            ay = wy - yOffset;
-            az = wz + offsetZ;
-        }
-        return new BlockPos(ax, ay, az);
-    }
+  public static long forwardPacked(TransitionGeometry geo, BlockPos pos) {
+    return geo.forwardPacked(pos);
+  }
 
-    public static BlockPos targetToSource(BlockPos targetPos, int offsetX, int yOffset, int offsetZ, boolean inverted, int maxY) {
-        int wx = targetPos.getX() - offsetX;
-        int wy;
-        int wz;
-        if (inverted) {
-            SkyPortalEventInstance portal = SkyPortalManager.getActive();
-            if (portal != null) {
-                wy = portal.crackPlaneY() + portal.crackPlaneYTarget() - targetPos.getY();
-                wz = 2 * portal.center().getZ() - targetPos.getZ();
-            } else {
-                wy = maxY - targetPos.getY();
-                wz = targetPos.getZ() - offsetZ;
-            }
-        } else {
-            wy = targetPos.getY() + yOffset;
-            wz = targetPos.getZ() - offsetZ;
-        }
-        return new BlockPos(wx, wy, wz);
-    }
-
-    public static long targetToSourcePacked(BlockPos targetPos, int offsetX, int yOffset, int offsetZ, boolean inverted, int maxY) {
-        int wx = targetPos.getX() - offsetX;
-        int wy;
-        int wz;
-        if (inverted) {
-            SkyPortalEventInstance portal = SkyPortalManager.getActive();
-            if (portal != null) {
-                wy = portal.crackPlaneY() + portal.crackPlaneYTarget() - targetPos.getY();
-                wz = 2 * portal.center().getZ() - targetPos.getZ();
-            } else {
-                wy = maxY - targetPos.getY();
-                wz = targetPos.getZ() - offsetZ;
-            }
-        } else {
-            wy = targetPos.getY() + yOffset;
-            wz = targetPos.getZ() - offsetZ;
-        }
-        return BlockPos.asLong(wx, wy, wz);
-    }
-
-    public static long sourceToTargetPacked(BlockPos sourcePos, int offsetX, int yOffset, int offsetZ, boolean inverted, int maxY) {
-        int ax = sourcePos.getX() + offsetX;
-        int ay;
-        int az;
-        if (inverted) {
-            SkyPortalEventInstance portal = SkyPortalManager.getActive();
-            if (portal != null) {
-                ay = portal.crackPlaneY() + portal.crackPlaneYTarget() - sourcePos.getY();
-                az = 2 * portal.center().getZ() - sourcePos.getZ();
-            } else {
-                ay = maxY - sourcePos.getY();
-                az = sourcePos.getZ() + offsetZ;
-            }
-        } else {
-            ay = sourcePos.getY() - yOffset;
-            az = sourcePos.getZ() + offsetZ;
-        }
-        return BlockPos.asLong(ax, ay, az);
-    }
+  public static long inversePacked(TransitionGeometry geo, BlockPos pos) {
+    return geo.inversePacked(pos);
+  }
 }
