3e800eb7a1f583f5d2f460d175203d64148f7eb5
Crown91 <161074891+Crown91@users.noreply.github.com>
2026-08-16 16:06:13 +0300

    update: upstream sync (AlphaBoatModel, ActiveZoneEvent, NostalgiaConfig, AlphaWorldGenLiquids, S2CStartTransitionVisualsPayload)

--- src/main/java/net/nostalgia/client/NostalgiaConfig.java
+++ src/main/java/net/nostalgia/client/NostalgiaConfig.java
@@ -2,52 +2,56 @@ package net.nostalgia.client;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.fabricmc.loader.api.FabricLoader;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class NostalgiaConfig {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final File FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "nostalgia_client.json");
+  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+  private static final File FILE = new File(FabricLoader.getInstance().getConfigDir().toFile(), "nostalgia_client.json");
+  public boolean alphaLoadingScreen = true;
+  public NostalgiaConfig.RitualType ritualType = NostalgiaConfig.RitualType.CLASSIC;
+  private static NostalgiaConfig instance;
 
-    public enum RitualType {
-        CLASSIC,
-        SEAMLESS_PORTAL
+  public NostalgiaConfig() {
+  }
+
+  public static NostalgiaConfig get() {
+    if (instance == null) {
+      load();
     }
 
-    public boolean alphaLoadingScreen = true;
-    public RitualType ritualType = RitualType.CLASSIC;
+    return instance;
+  }
 
-    private static NostalgiaConfig instance;
-
-    public static NostalgiaConfig get() {
-        if (instance == null) {
-            load();
-        }
-        return instance;
+  public static void load() {
+    if (FILE.exists()) {
+      try (FileReader reader = new FileReader(FILE)) {
+        instance = (NostalgiaConfig)GSON.fromJson(reader, NostalgiaConfig.class);
+      } catch (Exception var5) {
+        var5.printStackTrace();
+        instance = new NostalgiaConfig();
+      }
+    } else {
+      instance = new NostalgiaConfig();
+      save();
     }
+  }
 
-    public static void load() {
-        if (FILE.exists()) {
-            try (FileReader reader = new FileReader(FILE)) {
-                instance = GSON.fromJson(reader, NostalgiaConfig.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-                instance = new NostalgiaConfig();
-            }
-        } else {
-            instance = new NostalgiaConfig();
-            save();
-        }
+  public static void save() {
+    try (FileWriter writer = new FileWriter(FILE)) {
+      GSON.toJson(instance, writer);
+    } catch (Exception var5) {
+      var5.printStackTrace();
     }
+  }
 
-    public static void save() {
-        try (FileWriter writer = new FileWriter(FILE)) {
-            GSON.toJson(instance, writer);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+  public static enum RitualType {
+    CLASSIC,
+    SEAMLESS_PORTAL;
+
+    private RitualType() {
     }
+  }
 }
